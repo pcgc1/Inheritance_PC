4f435c235ed2fe29242986bbf7585b18f711de43
pcgc1 <chalutphilip@gmail.com>
2024-12-05 15:24:27 -0700

    baen

---
+++ src/Ex1_Animals/Ex1_Main.java
@@ -0,0 +1,32 @@
+package Ex1_Animals;
+
+import java.util.ArrayList;
+
+public class Ex1_Main {
+
+    public static void run(){
+        ArrayList<Animal> allAnimals = new ArrayList<>();
+
+        allAnimals.add(new Animal("Peppy") );
+        allAnimals.add( new Dog("Scout") );
+        allAnimals.add(new Rotweiller( "Rubble" ));
+        allAnimals.add( new Shihtzu("Bubbles"));
+        allAnimals.add (new Trout( "Slippery" ));
+
+//        for (int i = 0; i < allAnimals.size(); i++) {
+//            allAnimals.get(i).talk();
+//        }
+
+        System.out.println("What does the dog say?");
+
+        for (int i = 0; i < allAnimals.size(); i++) {
+            if( allAnimals.get(i) instanceof Dog ){
+                allAnimals.get(i).talk();
+            }
+        }
+
+
+    }//run
+
+
+}//class
