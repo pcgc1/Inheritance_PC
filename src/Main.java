8012f4b606c9e4187f13e9ac359509a0929f1693
pcgc1 <chalutphilip@gmail.com>
2024-12-05 15:24:13 -0700

    baen

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import Ex1_Animals.Ex1_Main;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Inheritance Project");
+        Ex1_Main.run();
 
     }
 
