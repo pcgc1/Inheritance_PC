48ed820b698fcd726e8107ebfb7dd189303f1a8b
pcgc1 <chalutphilip@gmail.com>
2024-12-13 15:01:46 -0700

    baen

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import Ex1_Animals.Ex1_Main;
 import Ex2_Restaurant.Ex2_Main;
+import Ex3_MusicFestival.Ex3_Main;
 
 public class Main {
     public static void main(String[] args) {
 
 //        Ex1_Main.run();
-        Ex2_Main.run();
+//        Ex2_Main.run();
+        Ex3_Main.run();
     }
 
 }
