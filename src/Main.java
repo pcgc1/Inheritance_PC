e2d629599f563e61d29ffe2d9e6f662958cdaa85
pcgc1 <chalutphilip@gmail.com>
2025-01-06 15:25:28 -0700

    baen

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 import Ex1_Animals.Ex1_Main;
 import Ex2_Restaurant.Ex2_Main;
 import Ex3_MusicFestival.Ex3_Main;
+import TwoDArrays.Ex1_GolfCard;
 
 public class Main {
     public static void main(String[] args) {
 
 //        Ex1_Main.run();
 //        Ex2_Main.run();
-        Ex3_Main.run();
+//        Ex3_Main.run();
+        Ex1_GolfCard.run();
     }
 
 }
