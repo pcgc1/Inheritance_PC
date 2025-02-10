ccb326552f1b31ba539f7143ee702b49cb831fd4
pcgc1 <chalutphilip@gmail.com>
2025-02-10 15:05:50 -0700

    baen

--- src/TwoDArrays/As2_Vigenere.java
+++ src/TwoDArrays/As2_Vigenere.java
@@ -39,18 +39,15 @@ public class As2_Vigenere {
 
 
     public static void printSquare(char[][] square) {
-        char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K',
-                'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-
 
         System.out.print("  ");
-        for (int i = 0; i < alphabet.length; i++) {
-            System.out.print("\u001B[33m" + alphabet[i] + " ");
+        for (int i = 0; i < square.length; i++) {
+            System.out.print("\u001B[33m" + square[i][0] + " ");
         }
         System.out.println(" ");
 
         for (int row = 0; row < square.length; row++) {
-            System.out.print("\u001B[33m" + alphabet[row] + " ");
+            System.out.print("\u001B[33m" + square[row][0] + " ");
             for (int col = 0; col < square[row].length; col++) {
                 System.out.print("\u001B[0m" + square[row][col] + " ");
             }
